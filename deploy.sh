8e01f07ea4d82cfbb33674564359231bfe611170
kaio <kaio@zhaw.ch>
2018-09-11 16:23:09 +0200

    OpenShift Deployment + Health checks

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,16 @@
 if [ $# = 0 ] || [ $1 = "-h" ] || [ $1 = "--help" ]; then
     echo -e "\e[93mCloudFoundry\e[39m deployment: ./deploy.sh cf [APP_NAME]"
     echo -e "\e[93mKubernetes\e[39m deployment: ./deploy.sh k8s"
+    echo -e "\e[OpenShift\e[39m deployment: ./deploy.sh os"
     exit
+fi
+
+# Choose between k8s or openshift
+BIN="none"
+if [ $1 = "k8s" ]; then
+    BIN="kubectl"  
+elif [ $1 = "os" ]; then
+    BIN="oc"
 fi
 
 if [ $1 = "cf" ]; then
@@ -11,15 +20,15 @@ if [ $1 = "cf" ]; then
         exit
     fi
     cf push $2 -f "manifest.yml" --vars-file="vars-file.yml"
-elif [ $1 = "k8s" ]; then
+elif [ $BIN != "none" ]; then
     echo -e "\e[93mUpdating ConfigMap\e[39m"
-    kubectl apply -f "k8s-configMap.yaml"
+    $BIN apply -f "k8s-configMap.yaml"
 
     echo -e "\e[93mUpdating Deployment\e[39m"
-    kubectl apply -f "k8s-deployment.yaml"
+    $BIN apply -f "k8s-deployment.yaml"
 
     echo -e "\e[93mUpdating Deployment\e[39m"
-    kubectl apply -f "k8s-service.yaml"
+    $BIN apply -f "k8s-service.yaml"
 else
     echo "Command '$1' not found."
     echo "Run './deploy.sh -h' for help"
