a2ffa89ae5c35a62c749641d57f8aebfbc423ab8
kaio <kaio@zhaw.ch>
2018-09-24 09:02:03 +0200

    Removed dead code

--- broker/utils.go
+++ broker/utils.go
@@ -75,32 +75,3 @@ func (b *Broker) getInstanceObjName(instID string) string {
 func (b *Broker) getBindObjName(instID string, bindID string) string {
 	return b.BrokerConfig.InstancePrefix + instID + "/" + bindID
 }
-
-func sliceContains(needle string, haystack []string) bool {
-	for _, element := range haystack {
-		if element == needle {
-			return true
-		}
-	}
-	return false
-}
-
-func getElementIndex(s string, slice []string) int {
-	for i, x := range slice {
-		if s == x {
-			return i
-		}
-	}
-
-	return -1
-}
-
-func removeFromSlice(s string, slice []string) []string {
-	i := getElementIndex(s, slice)
-
-	length := len(slice)
-	t := slice[length-1]
-	slice[length-1] = slice[i]
-	slice[i] = t
-	return slice[:length-1]
-}
